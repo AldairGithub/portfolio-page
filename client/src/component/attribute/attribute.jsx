05195d8c2808988ad2000d0394b62066aafc480e
acarneiro <acarneiro1413@gmail.com>
2020-09-02 09:56:13 -0400

    added links to portfolio component

--- client/src/component/attribute/attribute.jsx
+++ client/src/component/attribute/attribute.jsx
@@ -12,8 +12,8 @@ export default function Attribute(props) {
         <div className='attribution'>
           <span>Photo by <a href="https://unsplash.com/@dancalders?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target='_blank' rel="noopener noreferrer">Dan Calderwood</a> on <a href="https://unsplash.com/backgrounds/art/smoke?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target='_blank' rel="noopener noreferrer">Unsplash</a></span>
         </div>
-        <div className='attribution'>
-          Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target='_blank' rel="noopener noreferrer">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" target='_blank' rel="noopener noreferrer"> www.flaticon.com</a>
+        <div className='copyright'>
+          <h4>&copy; 2020 Aldair Carneiro</h4>
         </div>
       </div>
     </>
--- client/src/component/portfolio/data/data.jsx
+++ client/src/component/portfolio/data/data.jsx
@@ -6,12 +6,26 @@ export default function Data(props) {
   const { img } = props
   const { title } = props
   const { description } = props
+  const { siteLink } = props
+  const { codeLink } = props
   return (
     <div className='dataContainer'>
       <img className='portfolioImg' src={img} alt={title}/>
       <div className='textContainer'>
         <h1>{title}</h1>
         <p>{description}</p>
+        <div className='portfolio-button-container'>
+          <a href={siteLink}
+            target='_blank'
+            rel='noopener noreferrer'>
+            <button className='portfolio-button'>Site</button>
+          </a>
+          <a href={codeLink}
+            target='_blank'
+            rel='noopener noreferrer'>
+            <button className='portfolio-button'>Code</button>
+          </a>
+        </div>
       </div>
     </div>
   )
