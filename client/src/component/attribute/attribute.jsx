9eba815a5de21ab11447b4277b60e9da22b97100
acarneiro <acarneiro1413@gmail.com>
2020-09-03 13:46:45 -0400

    added media queries for tablets

--- client/src/component/attribute/attribute.jsx
+++ client/src/component/attribute/attribute.jsx
@@ -4,18 +4,18 @@ import './attribute.css'
 export default function Attribute(props) {
   const attrVisible = props.isBottom ? 'visible' : 'not-visible'
   return (
-    <>
-      <div className={`${attrVisible}`}>
+    <div className={`${attrVisible}`}>
+      <div className='attribute-container'>
         <div className='attribution'>
           <span>Favicon by <a href='https://www.linkedin.com/in/kelly-marray/' target='_blank' rel="noopener noreferrer">Kelly Marray</a></span>
         </div>
         <div className='attribution'>
           <span>Photo by <a href="https://unsplash.com/@dancalders?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target='_blank' rel="noopener noreferrer">Dan Calderwood</a> on <a href="https://unsplash.com/backgrounds/art/smoke?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target='_blank' rel="noopener noreferrer">Unsplash</a></span>
         </div>
-        <div className='copyright'>
-          <h4>&copy; 2020 Aldair Carneiro</h4>
-        </div>
       </div>
-    </>
+      <div className='copyright'>
+        <h4>&copy; 2020 Aldair Carneiro</h4>
+      </div>
+    </div>
   )
 }
